0eeb952926453663a3ac6a35dc20c284712a46c0
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2021-07-15 15:51:16 +0200

    Updated Icons for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,22 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {faGithub,faLinkedin,faSafari,} from "@fortawesome/free-brands-svg-icons";
-//import links from "../assets/links.json";
-
-
 export default function Footer() {
   return (
     <footer>
       <ol>
         <li>
           <a href= "mailto:sailathatammana1997@gmail.com" target="blank" className="icon">
-            <FontAwesomeIcon icon={faEnvelope} className="icon " size="2x" />
+          <i class="fas fa-envelope" aria-hidden="true"></i>
           </a>
         </li>
 
         <li>
           <a href= "https://github.com/sailathatammana" target="blank">
-            <FontAwesomeIcon icon={faGithub} className="icon " size="2x" />
+          <i class="fab fa-fw fa-github" aria-hidden="true"></i>
           </a>
         </li>
 
         <li>
           <a href= "https://www.linkedin.com/in/sai-latha-tammana/" target="blank">
-            <FontAwesomeIcon icon={faLinkedin} className="icon " size="2x" />
+          <i class="fab fa-fw fa-linkedin-in" aria-hidden="true"></i>
           </a>
         </li>
       </ol>
