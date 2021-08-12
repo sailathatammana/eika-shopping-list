2c1ad899cfc64423d676e67a1c09de928b9053f9
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2021-08-12 16:01:55 +0200

    Symbol visibility implemented

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,3 +1,5 @@
+import { Eye, EyeSlash } from "react-bootstrap-icons";
+
 export default function Sort({
   sortBy,
   sortByName,
@@ -60,7 +62,19 @@ export default function Sort({
             onChange={toggleFilter}
           />
           <label className="label-for-check" htmlFor="acquired">
-            Acquired
+            <div className="wrapper">
+              {filterResults ? (
+                <>
+                  <EyeSlash className="btn btn-sm icon" />
+                  <p>Go back</p>
+                </>
+              ) : (
+                <>
+                  <Eye className="btn btn-sm icon" />
+                  <p>Show Acquired</p>
+                </>
+              )}
+            </div>
           </label>
         </div>
       </div>
